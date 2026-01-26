e1652f445941676c39dbc4d4fa42dedaf799f4cc
Денис А. Жилин <zilin.d@sm.local>
2026-01-26 16:31:53 +0300

    CRUD hw 1.2

--- dz/4-order-api/cmd/main.go
+++ dz/4-order-api/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"4-order-api/config"
 	"4-order-api/database"
+	"4-order-api/internal/product"
 	"log"
 	"net/http"
 )
@@ -27,7 +28,11 @@ func main() {
 		log.Fatalf("Migration error: %v", err)
 	}
 
-	// 4. Настройка HTTP сервера (пока пустой)
+	// 4. Создаем handler для продуктов
+
+	productHandler := product.NewHandler(db)
+
+	// 5. Настройка HTTP сервера (пока пустой)
 
 	mux := http.NewServeMux()
 
@@ -35,6 +40,11 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	mux.HandleFunc("POST /products", productHandler.CreateProduct)
+	mux.HandleFunc("GET /products", productHandler.GetProduct)
+	mux.HandleFunc("PUT /products", productHandler.UpdateProduct)
+	mux.HandleFunc("DELETE /products", productHandler.DeleteProduct)
 
 	log.Printf("Server starting on %s", cfg.ServerPort)
 	if err := http.ListenAndServe(cfg.ServerPort, mux); err != nil {
